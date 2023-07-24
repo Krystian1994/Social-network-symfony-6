09fc5dee09bc715f9ddc4f2ec3cd6529f805a95c
Krystian <krystian.surowiec.programista@gmail.com>
2023-07-24 14:36:58 +0200

    forms

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -2,12 +2,15 @@
 
 namespace App\Controller;
 
-use App\Entity\MicroPost;
-use App\Repository\MicroPostRepository;
 use DateTime;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\MicroPost;
+use App\Form\MicroPostType;
+use App\Repository\MicroPostRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MicroPostController extends AbstractController
 {
@@ -32,6 +35,60 @@ class MicroPostController extends AbstractController
         // dd($post);
         return $this->render('micro_post/show.html.twig', [
             'post' => $post,
+        ]);
+    }
+
+    #[Route('/micro-post/add', name: 'app_micro_post_add', priority: 2)]
+    public function add(Request $request, MicroPostRepository $posts): Response
+    {
+        // $microPost = new MicroPost;
+        // $form = $this->createFormBuilder($microPost)
+        // ->add('title')
+        // ->add('text')
+        // // ->add('submit', SubmitType::class, ['label' => 'save'])
+        // ->getForm();
+
+        $form = $this->createForm(MicroPostType::class, new MicroPost);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+            $post->setCreated(new DateTime());
+            $posts->add($post, true);
+
+            //add flash
+            $this -> addFlash('success', 'Your micro post has been add');
+            //redirect
+            return $this -> redirectToRoute('app_micro_post');
+        }
+
+        return $this->render('micro_post/add.html.twig', [
+            'form' => $form
+        ]);
+    }
+
+    #[Route('/micro-post/{post}/edit', name: 'app_micro_post_edit')]
+    public function edit(MicroPost $post, Request $request, MicroPostRepository $posts): Response
+    {
+        // $form = $this->createFormBuilder($post)
+        // ->add('title')
+        // ->add('text')
+        // ->getForm();
+        $form = $this->createForm(MicroPostType::class, $post);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+            $posts->add($post, true);
+
+            //add flash
+            $this -> addFlash('success', 'Your micro post has been changed');
+            //redirect
+            return $this -> redirectToRoute('app_micro_post');
+        }
+
+        return $this->render('micro_post/edit.html.twig', [
+            'form' => $form
         ]);
     }
 }
