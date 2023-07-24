65cdfae27043c4208c861cb7f97207675f32dfe5
Krystian <krystian.surowiec.programista@gmail.com>
2023-07-24 11:49:35 +0200

    Databases and Doctrine ORM

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -2,17 +2,36 @@
 
 namespace App\Controller;
 
+use App\Entity\MicroPost;
+use App\Repository\MicroPostRepository;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MicroPostController extends AbstractController
 {
-    #[Route('/micro/post', name: 'app_micro_post')]
-    public function index(): Response
+    #[Route('/micro-post', name: 'app_micro_post')]
+    public function index(MicroPostRepository $posts): Response
     {
+        // $microPost = new MicroPost;
+        // $microPost->setTitle('Uwaga!');
+        // $microPost->setText('Ta modyfikacja pochodzi z poziomu kontrollera w php !!');
+        // $microPost->setCreated(new DateTime());
+        // $posts->add($microPost, true);
+
+        // dd($posts->findAll());
         return $this->render('micro_post/index.html.twig', [
-            'controller_name' => 'MicroPostController',
+            'posts' => $posts->findAll(),
+        ]);
+    }
+
+    #[Route('/micro-post/{post}', name: 'app_micro_post_show')]
+    public function showOne(MicroPost $post): Response
+    {
+        // dd($post);
+        return $this->render('micro_post/show.html.twig', [
+            'post' => $post,
         ]);
     }
 }
